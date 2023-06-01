b0ad8221a819add8f29e2aefc50f0aa11abf1a35
johnny <jmalulan7@gmail.com>
2023-06-01 21:54:21 +0800

    Add fetch prompts and prompt card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,10 +3,35 @@
 import { useState, useEffect } from "react";
 import PromptCard from "@components/PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    //Call the function
+    fetchPosts();
+  }, []);
 
   return (
     <section className="feed">
@@ -20,6 +45,7 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
