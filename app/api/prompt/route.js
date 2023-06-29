4495e79b827f3c4b8895ce66f7dfbd40afe9312d
johnny <jmalulan7@gmail.com>
2023-06-29 17:48:52 +0800

    Add revalidate to api/prompt

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,21 +1,14 @@
 import Prompt from "@models/prompt";
 import { connectToDatabase } from "@utils/database";
 
+export const revalidate = 1; //revalidate api every 1 second
 export const GET = async (request) => {
   try {
     await connectToDatabase();
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    const headers = {
-      "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
-    };
-
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      revalidate: 0,
-      headers,
-    });
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
